775915886f30bc8a13e67a0dcbc1fdb33fc18ef7
LAZAAR Youssef <lazaar.youssef0@gmail.com>
2024-11-09 04:00:41 +0100

    premier controller

---
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.sip.ams.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller 
+public class HomeController {
+	
+@RequestMapping("/home")
+@ResponseBody
+	public String home()
+	{
+		return "<h2>Bienvenue au BootCamp<h2>";
+	}
+}
